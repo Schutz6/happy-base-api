fcec48759e9816559aad5b7f33100a4166855aff
Schutz <happycoding996@gmail.com>
2023-06-15 10:47:36 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,34 @@
 import asyncio
 
+from tornado import locks
 from base.utils import mongo_helper
+
+lock = locks.Lock()
 
 
 async def demo_transaction():
-    # 事务测试代码
+    # 事务控制代码
     async with await mongo_helper.connect_client.start_session() as s:
         async with s.start_transaction():
             await mongo_helper.insert_one_session("Demo", {"name": "张三", "sex": "男"}, s)
             await mongo_helper.insert_one_session("Demo", {"name": "李四", "sex": "女"}, s)
             raise Exception("出错了")
 
-if __name__ == '__main__':
+
+async def demo_concurrency():
+    # 并发加锁代码
+    async with lock:
+        user = await mongo_helper.fetch_one("User", {"_id": 100002})
+        balance = user.get("balance", 0)
+        balance += 1
+        await mongo_helper.update_one("User", {"_id": 100002}, {"$set": {"balance": balance}})
+
+
+async def demo():
     # 事务代码
-    asyncio.run(demo_transaction())
+    await demo_transaction()
+
+
+if __name__ == '__main__':
+    # 启动测试代码
+    asyncio.run(demo())
